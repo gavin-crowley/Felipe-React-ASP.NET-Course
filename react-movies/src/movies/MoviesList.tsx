cddb2827881bdac491f71cef9c67cb455b3f100f
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-01-23 12:19:37 +0000

    Generic List

--- react-movies/src/movies/MoviesList.tsx
+++ react-movies/src/movies/MoviesList.tsx
@@ -1,21 +1,18 @@
-import Loading from "../utils/Loading";
+import GenericList from "./../utils/GenericList";
 import IndividualMovie from "./IndividualMovie";
 import { movieDTO } from "./movies.model";
 import css from './MoviesList.module.css'
 
 function MoviesList(props: moviesListProps) {
-
-    if (!props.movies) {
-        return <Loading />
-    } else if (props.movies.length === 0) {
-        return <>There are no movies to display.</>
-    } else {
-        return (<div className={css.div}>
-            {props.movies.map(movie =>
+    return <GenericList 
+    // loadingUI={<>Loading...</>}
+    list={props.movies}>
+        <div className={css.div}>
+            {props.movies?.map(movie =>
                 <IndividualMovie {...movie} key={movie.id} />
             )}
-        </div>);
-    }
+        </div>
+    </GenericList>
 }
 
 interface moviesListProps {
